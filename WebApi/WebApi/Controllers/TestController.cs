2ed14d6d95e042916e8cecb28d771360003f5596
Carlos García <UO139393@uniovi.es>
2018-11-21 23:18:30 +0100

    New console an WebAPI apps

---
+++ WebApi/WebApi/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            return "¡Gracias " + id + "!";
+        }
+
+        [HttpPost("{id}")]
+        public string Post([FromBody]Dummy dum)
+        {
+            return dum.i + dum.s + dum.d;
+        }
+
+        [HttpPut("{id}")]
+        public void Put(string id)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+        }
+    }
+}
---
+++ WebApi/WebApi/Dummy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Dummy
+    {
+        public int i { get; set; }
+        public string s { get; set; }
+        public double d { get; set; }
+    }
+}
